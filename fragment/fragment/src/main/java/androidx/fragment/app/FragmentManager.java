7cbe759e0e809bbfb1ca31b0165ead1c980705e1
tribalfs <tribalfs@gmail.com>
2024-10-22 10:49:26 +0800

    Revert "Workaround for bug 342419080/358743378 until official fix is released"
    This reverts commit fc3d0c43
    
    Signed-off-by: tribalfs <tribalfs@gmail.com>

--- fragment/fragment/src/main/java/androidx/fragment/app/FragmentManager.java
+++ fragment/fragment/src/main/java/androidx/fragment/app/FragmentManager.java
@@ -117,7 +117,6 @@ public abstract class FragmentManager implements FragmentResultOwner {
     public static final String TAG = "FragmentManager";
 
     static boolean USE_PREDICTIVE_BACK = true;
-    ArrayList<BackStackRecord> mBackStackBackup = null;
 
     /**
      * Control whether FragmentManager uses the new state predictive back feature that allows
@@ -1072,13 +1071,6 @@ public abstract class FragmentManager implements FragmentResultOwner {
             executePendingTransactions();
             mHandlingTransitioningOp = false;
             mTransitioningOp = null;
-
-            //Workaround for bug 342419080/358743378 until official fix is released
-            mBackStack = new ArrayList<>(mBackStackBackup.size());
-            for (BackStackRecord record : mBackStackBackup) {
-                mBackStack.add(new BackStackRecord(record));
-            }
-            mBackStackBackup = null;
         }
     }
 
@@ -2604,23 +2596,6 @@ public abstract class FragmentManager implements FragmentResultOwner {
 
     boolean prepareBackStackState(@NonNull ArrayList<BackStackRecord> records,
             @NonNull ArrayList<Boolean> isRecordPop) {
-        if (FragmentManager.isLoggingEnabled(Log.VERBOSE)) {
-            Log.v(
-                    TAG, "FragmentManager has the following pending actions inside of "
-                            + "prepareBackStackState: " + mPendingActions
-            );
-        }
-        if (mBackStack.isEmpty()) {
-            Log.i(TAG, "Ignoring call to start back stack pop because the back stack is empty.");
-            return false;
-        }
-
-        //Workaround for bug 342419080/358743378 until official fix is released
-        mBackStackBackup =  new ArrayList<>(mBackStack.size());
-        for (BackStackRecord record : mBackStack) {
-            mBackStackBackup.add(new BackStackRecord(record));
-        }
-
         // The transitioning record is the last one on the back stack.
         mTransitioningOp = mBackStack.get(mBackStack.size() - 1);
         // Mark all fragments in the record as transitioning
