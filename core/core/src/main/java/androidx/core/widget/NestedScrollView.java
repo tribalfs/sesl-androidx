4ba4228c27d021cec1b41a1c68268e5b15c9a442
Jake Wharton <jw@squareup.com>
2024-11-27 22:25:46 +0800

    Deprecate BuildCompat.isAtLeastV and update callers
    Test: gw -p core bOS
    Test: gw -p credentials bOS
    Test: gw -p ink bOS
    Relnote: Deprecate BuildCompat.isAtLeastV. Callers should check SDK_INT against 35 directly instead.
    Change-Id: I294d117a8fea924e7f1b739d52268a9a54be6db7
    Signed-off-by: tribalfs <tribalfs@gmail.com>

--- core/core/src/main/java/androidx/core/widget/NestedScrollView.java
+++ core/core/src/main/java/androidx/core/widget/NestedScrollView.java
@@ -73,7 +73,6 @@ import androidx.annotation.RequiresApi;
 import androidx.annotation.RestrictTo;
 import androidx.annotation.VisibleForTesting;
 import androidx.core.R;
-import androidx.core.os.BuildCompat;
 import androidx.core.view.AccessibilityDelegateCompat;
 import androidx.core.view.DifferentialMotionFlingController;
 import androidx.core.view.DifferentialMotionFlingTarget;
@@ -2153,7 +2152,7 @@ public class NestedScrollView extends FrameLayout implements NestedScrollingPare
 
         final int range = getScrollRange();
 
-        if (BuildCompat.isAtLeastV()) {
+        if (Build.VERSION.SDK_INT >= 35) {
             Api35Impl.setFrameContentVelocity(NestedScrollView.this,
                     Math.abs(mScroller.getCurrVelocity()));
         }
@@ -2522,7 +2521,7 @@ public class NestedScrollView extends FrameLayout implements NestedScrollingPare
                     Integer.MIN_VALUE, Integer.MAX_VALUE, // y
                     0, 0); // overscroll
             runAnimatedScroll(true);
-            if (BuildCompat.isAtLeastV()) {
+            if (Build.VERSION.SDK_INT >= 35) {
                 Api35Impl.setFrameContentVelocity(NestedScrollView.this,
                         Math.abs(mScroller.getCurrVelocity()));
             }
