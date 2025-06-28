f17a9ffe95426878d1e07a140eb8a134fcd6451f
tribalfs <tribalfs@gmail.com>
2025-06-28 20:29:46 +0800

    drawerlayout: bump to 1.2.0+1.0.0-sesl8+rev0
    Signed-off-by: tribalfs <tribalfs@gmail.com>

--- manifest.gradle.kts
+++ manifest.gradle.kts
@@ -9,7 +9,7 @@ extra.apply {
             "androidx.core" to listOf("1.16.0", "1.0.7-sesl8", "rev0", 21, 35),
             "androidx.core-ktx" to listOf("1.16.0", "1.0.0-sesl8", "rev0", 21, 35),
             "androidx.customview" to listOf("1.2.0-rc01", "1.0.0-sesl8", "rev0", 21, 35),
-            "androidx.drawerlayout" to listOf("1.2.0", "1.0.0-sesl7", "rev0", 21, 35),
+            "androidx.drawerlayout" to listOf("1.2.0", "1.0.0-sesl8", "rev0", 21, 35),
             "androidx.viewpager" to listOf("1.1.0-beta01", "1.0.1-sesl7", "rev0", 21, 35),
             "androidx.coordinatorlayout" to listOf("1.3.0", "1.0.0-sesl7", "rev0", 21, 35),
             "androidx.appcompat" to listOf("1.7.1", "1.0.47000-sesl7", "rev0", 21, 35),
