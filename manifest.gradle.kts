10056b102dc38e09fdcb49e371cb46638a62855a
tribalfs <tribalfs@gmail.com>
2025-08-16 03:25:03 +0800

    fragment: bump to 1.8.9+1.0.5-sesl8+rev0
    Signed-off-by: tribalfs <tribalfs@gmail.com>

--- manifest.gradle.kts
+++ manifest.gradle.kts
@@ -13,7 +13,7 @@ extra.apply {
             "androidx.viewpager" to listOf("1.1.0-beta01", "1.0.0-sesl8", "rev0", 21, 35),
             "androidx.coordinatorlayout" to listOf("1.3.0", "1.0.0-sesl8", "rev0", 21, 35),
             "androidx.appcompat" to listOf("1.7.1", "1.0.21-sesl8", "rev0", 21, 35),
-            "androidx.fragment" to listOf("1.8.8", "1.0.5-sesl8", "rev0", 21, 35),
+            "androidx.fragment" to listOf("1.8.9", "1.0.5-sesl8", "rev0", 21, 35),
             "androidx.recyclerview" to listOf("1.4.0", "1.0.12-sesl8", "rev0", 21, 35),
             "androidx.preference" to listOf("1.2.1", "1.0.0-sesl8", "rev0", 21, 35),
             "androidx.slidingpanelayout" to listOf("1.2.0", "1.0.4-sesl8", "rev0", 21, 35),
