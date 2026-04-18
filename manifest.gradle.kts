824560d10da6fa2434453cba3273334da014bd26
tribalfs <tribalfs@gmail.com>
2026-04-18 20:42:43 +0800

    slidingpanelayout: bump to 1.2.0+1.0.4-sesl8+rev1
    Signed-off-by: tribalfs <tribalfs@gmail.com>

--- manifest.gradle.kts
+++ manifest.gradle.kts
@@ -16,7 +16,7 @@ extra.apply {
             "androidx.fragment" to listOf("1.8.9", "1.0.5-sesl8", "rev1", 21, 35),
             "androidx.recyclerview" to listOf("1.4.0", "1.0.12-sesl8", "rev3", 21, 35),
             "androidx.preference" to listOf("1.2.1", "1.0.0-sesl8", "rev1", 21, 35),
-            "androidx.slidingpanelayout" to listOf("1.2.0", "1.0.4-sesl8", "rev0", 21, 35),
+            "androidx.slidingpanelayout" to listOf("1.2.0", "1.0.4-sesl8", "rev1", 21, 35),
             "androidx.viewpager2" to listOf("1.1.0", "1.0.0-sesl8", "rev0", 21, 35),
             "androidx.swiperefreshlayout" to listOf("1.2.0-alpha01", "1.0.0-sesl8", "rev0", 21, 35),
             "androidx.indexscroll" to listOf("1.0.0", "1.0.0-sesl8", "rev0", 21, 35),
