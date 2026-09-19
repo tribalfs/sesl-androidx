d41ea98aeb771de44ed94a3e0c16d80da2d42a44
tribalfs <tribalfs@gmail.com>
2026-09-20 03:46:24 +0800

    recyclerview: release 1.4.0+1.0.25-sesl9+rev1
    Signed-off-by: tribalfs <tribalfs@gmail.com>

--- buildSrc/src/main/kotlin/SeslManifest.kt
+++ buildSrc/src/main/kotlin/SeslManifest.kt
@@ -28,7 +28,7 @@ object SeslManifest {
         "androidx.coordinatorlayout" to listOf("1.3.0", "1.0.6-sesl9", "rev0", 23, 37),
         "androidx.appcompat" to listOf("1.8.0", "1.0.38-sesl9", "rev0", 23, 37),
         "androidx.fragment" to listOf("1.9.0", "1.0.6-sesl9", "rev1", 23, 37),
-        "androidx.recyclerview" to listOf("1.4.0", "1.0.25-sesl9", "rev0", 23, 37),
+        "androidx.recyclerview" to listOf("1.4.0", "1.0.25-sesl9", "rev1", 23, 37),
         "androidx.preference" to listOf("1.2.1", "1.0.5-sesl9", "rev0", 23, 37),
         "androidx.slidingpanelayout" to listOf("1.2.0", "1.0.2-sesl9", "rev0", 23, 37),
         "androidx.viewpager2" to listOf("1.1.0", "1.0.5-sesl9", "rev0", 23, 37),
